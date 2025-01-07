f5672c50fcd594cca6d7747b0031f006faa97009
shafiqps <u2000428@siswa.um.edu.my>
2025-01-08 05:05:41 +0800

    Add IAM policy binding step to workflow and allows all origins for CORS

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ create_tables()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_origins=["*"],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
